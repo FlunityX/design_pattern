78c3f51cfe790ab20dd9099784b1f60d44400b64
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-11-22 21:49:43 +0700

    kĩ thuật design pattern

---
+++ Factory Method/factory_method/kemdau.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_method
+{
+    internal class kemdau : Isanphamkem
+    {
+        public void loaikem()
+        {
+            Console.WriteLine(" day la kem dau ");
+        } 
+    }
+}
---
+++ Factory Method/factory_method/kemvani.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_method
+{
+    internal class kemvani:Isanphamkem
+    {
+        public void loaikem()
+        {
+            Console.WriteLine(" day la kem vani ");
+        }
+    }
+}
