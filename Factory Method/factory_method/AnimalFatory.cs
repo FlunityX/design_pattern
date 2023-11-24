68c72de5ace3f5f5323f3e16cc93731619326ef5
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-11-25 00:07:24 +0700

    design pattern

---
+++ Factory Method/factory_method/AnimalFatory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_method
+{
+    public class basicFactory {
+        public IAnimal CreateAnimal(string animalType)
+        {
+            switch (animalType)
+            {
+                case "Cat":
+                    return new Cat();
+                case "Chicken":
+                    return new Chicken();
+                default:
+                    throw new ArgumentException("Invalid animal type.");
+            }
+        }
+    }
+    public class randomFatory
+    {
+        private static Random random = new Random();
+
+        public static IAnimal CreateRandomAnimal()
+        {
+            int randomNumber = random.Next(0, 3);
+
+            switch (randomNumber)
+            {
+                case 0:
+                    return new Dog();
+                case 1:
+                    return new Cat();
+                case 2:
+                    return new Chicken();
+                default:
+                    throw new ArgumentException("Invalid animal type.");
+            }
+        }
+    }
+}
---
+++ Factory Method/factory_method/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace factory_method
+{
+    public interface IAnimal
+    {
+        public void Sound();
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            basicFactory chonloai = new basicFactory();
+            IAnimal cat = chonloai.CreateAnimal("Cat");
+            cat.Sound(); 
+            IAnimal chicken = chonloai.CreateAnimal("Chicken");
+            chicken.Sound(); 
+            randomFatory animalFactory = new randomFatory();
+            IAnimal loai2 = randomFatory.CreateRandomAnimal();
+            loai2.Sound();
+            Console.ReadLine();
+
+        }
+    }
+}
