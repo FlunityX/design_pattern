ad602c1d3c71b570637d5d8be1423fc80013a07f
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-12-10 23:38:48 +0700

    TemplateMethod

---
+++ template.Method/bài tập/Program.cs
@@ -0,0 +1,70 @@
+﻿using luyện_template.Method;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luyện_template.Method
+{
+    public abstract class TextProcessor
+    {
+        public void Process() {
+            PlainTextProcessor();
+            HtmlProcessor();
+            MarkdownProcessor();
+        }
+        protected abstract void MarkdownProcessor();
+        protected abstract void PlainTextProcessor();
+        protected abstract void HtmlProcessor();
+     
+    }
+    public class Text_1 : TextProcessor
+    {
+        protected override void HtmlProcessor()
+        {
+            Console.WriteLine("xử lý Html của loại tệp thứ nhất");
+        }
+        protected override void MarkdownProcessor()
+        {
+            Console.WriteLine("xử lý markdown của loại tệp thứ nhất");
+        }
+        protected override void PlainTextProcessor()
+        {
+            Console.WriteLine("xử lý PlainText của loại tệp thứ nhất");
+        }
+    }
+    public class  Text_2 : TextProcessor
+    {
+        protected override void HtmlProcessor()
+        {
+            Console.WriteLine("xử lý Html của loại tệp thứ hai");
+        }
+        protected override void MarkdownProcessor()
+        {
+            Console.WriteLine("xử lý markdown của loại tệp thứ hai ");
+        }
+        protected override void PlainTextProcessor()
+        {
+            Console.WriteLine("xử lý PlainText  của loại tệp thứ hai");
+        }
+    }
+
+}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.ForegroundColor=ConsoleColor.Red;
+        TextProcessor tep1 = new Text_1();
+        tep1.Process();
+        Console.ResetColor();
+        Console.WriteLine();
+        Console.ForegroundColor=ConsoleColor.Green;
+        TextProcessor tep2 = new Text_2();
+        tep1.Process();
+        Console.ResetColor();
+        Console.ReadLine();
+
+    }
+    }
---
+++ template.Method/ví dụ/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+// Đây là một lớp trừu tượng định nghĩa giao diện cơ bản cho một trò chơi.
+// Nó chứa một phương thức không thể triển khai được gọi là Play(),
+// cũng như các phương thức trừu tượng khác Initialize(), StartGame(), và EndGame().
+public abstract class Game
+{
+    //public void Play(): Phương thức này triển khai logic chung cho việc chơi một trò chơi
+    // Nó gọi các phương thức trừu tượng để thực hiện các bước khởi tạo, bắt đầu trò chơi và kết thúc trò chơi.
+    // bóng đá hay bóng rổ có 3 phương thức con
+    public void Play()// phương thức mẫu - định nghĩa các phương thức // cấu trúc chung đối tg
+    {
+        Initialize();
+        StartGame();
+        EndGame();
+    }
+    // phương thức trừu tg để lớp con tiến hành mở rộng
+    protected abstract void Initialize();
+
+    protected abstract void StartGame();
+
+    protected abstract void EndGame();
+}
+// triển khai lớp con ghi đè lên các phương thức đó
+public class FootballGame : Game
+{
+    protected override void Initialize()
+    {
+        Console.WriteLine("Football game initialized");
+    }
+
+    protected override void StartGame()
+    {
+        Console.WriteLine("Football game started");
+    }
+
+    protected override void EndGame()
+    {
+        Console.WriteLine("Football game ended");
+    }
+}
+
+public class BasketballGame : Game
+{
+    protected override void Initialize()
+    {
+        Console.WriteLine("Basketball game initialized");
+    }
+
+    protected override void StartGame()
+    {
+        Console.WriteLine("Basketball game started");
+    }
+
+    protected override void EndGame()
+    {
+        Console.WriteLine("Basketball game ended");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Game footballGame = new FootballGame();
+        footballGame.Play();
+
+        Console.WriteLine();
+
+        Game basketballGame = new BasketballGame();
+        basketballGame.Play();
+        Console.ReadLine();
+    } }
