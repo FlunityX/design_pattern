3aa2854b8f27453c8291adf4021bee78df3f26ab
FlunityX <149858650+FlunityX@users.noreply.github.com>
2024-01-24 10:15:19 +0700

    prototype pattern

---
+++ prototype pattern/Class parent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototype_pattern
+{
+    public abstract class Class_parent
+    {
+        public abstract Class_parent Clone();
+    }
+    public class Class_clone :Class_parent 
+    {
+        private int id;
+
+        public Class_clone(int id)
+        {
+            this.id = id;
+        }
+        public void Display()
+        {
+            Console.WriteLine("ID: " + id);
+        }
+        public override Class_parent Clone()
+        {
+        return new Class_clone(this.id);
+        }
+    }
+}
---
+++ prototype pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace prototype_pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Class_clone sp = new Class_clone(2);
+            Class_clone Clone = (Class_clone)sp.Clone();
+            sp.Display();
+        }
+    }
+}
